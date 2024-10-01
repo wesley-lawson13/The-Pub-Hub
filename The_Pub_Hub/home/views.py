085b6b4e1c98a65bc9d1a07bfd9a00241aed83fc
Wesley Lawson <wesleylawson13@gmail.com>
2024-10-01 20:01:10 +0100

    Created initial HTML layout and home files, added CSS styling to homepage.

--- The_Pub_Hub/home/views.py
+++ The_Pub_Hub/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "home/homepage.html")
+
+def posts(request):
+    return render(request, "home/posts.html")
