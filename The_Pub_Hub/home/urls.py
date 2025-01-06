b6979b4ef42e582f736328f2fa4925197d3f5640
Wesley Lawson <wesleylawson13@gmail.com>
2025-01-05 23:05:16 -0500

    Reworked CSS properties and added Bootstrap framework for ease of design.

--- The_Pub_Hub/home/urls.py
+++ The_Pub_Hub/home/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts", views.posts, name="posts")
+    path("posts", views.posts, name="posts"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("about", views.about, name="about")
 ]
--- The_Pub_Hub/home/views.py
+++ The_Pub_Hub/home/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def posts(request):
     return render(request, "home/posts.html")
+
+def login(request):
+    return render(request, "home/login.html")
+
+def register(request):
+    return render(request, "home/register.html")
+
+def about(request):
+    return render(request, "home/about.html")
